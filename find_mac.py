b0ca26e51611ab7df8e847495869e0d173891265
Bjørnar Lintvedt <bjornar.lintvedt@bluetree.no>
2021-03-24 11:25:42 +0100

    prettier and more informative print commands

--- find_mac.py
+++ find_mac.py
@@ -12,25 +12,31 @@ class MerakiApi:
             suppress_logging=True
         )
 
-    def _get_org_name(self, meraki_org_id):
+    def get_org_name(self):
         my_organizations = self.dashboard.organizations.getOrganizations()
         org_name = "default"
 
         for org in my_organizations:
-            if org['id'] == meraki_org_id:
+            if org['id'] == self.meraki_org_id:
                 org_name = org['name']
         return org_name
 
     def _get_networks(self):
         try:
-            networks = self.dashboard.organizations.getOrganizationNetworks(self.meraki_org_id)
+            networks = self.dashboard.organizations.getOrganizationNetworks(
+                self.meraki_org_id
+            )
             return networks
-        except meraki.exceptions.APIError:
+        except meraki.exceptions.APIError as e:
+            print(f'    ERROR: {e}')
             return None
 
     def _get_network_clients(self, network_id):
         try:
-            clients = self.dashboard.networks.getNetworkClients(network_id, perPage=1000)
+            clients = self.dashboard.networks.getNetworkClients(
+                network_id,
+                total_pages='all'
+            )
             return clients
         except meraki.exceptions.APIError:
             return None
@@ -63,7 +69,6 @@ class MerakiApi:
 
                 print("    MAC not found in this network")
         else:
-            print("Error: Please verify meraki api key and org id")
             return None
 
     def blink_device_leds(self, serial):
@@ -73,20 +78,25 @@ class MerakiApi:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest='MERAKI_KEY', help="Meraki API key")
+    parser.add_argument(dest='MERAKI_API_KEY', help="Meraki API key")
     parser.add_argument(dest='MERAKI_ORG_ID', help="Meraki ORG id")
     parser.add_argument(dest='MAC_ADDRESS', help="MAC address to search for")
     args = parser.parse_args()
 
+    api_key = args.MERAKI_API_KEY
+    org_id = args.MERAKI_ORG_ID
+    mac = args.MAC_ADDRESS
+
     meraki_api = MerakiApi(
-        meraki_api_key=args.MERAKI_KEY,
-        meraki_org_id=args.MERAKI_ORG_ID,
+        meraki_api_key=api_key,
+        meraki_org_id=org_id,
     )
 
-    client = meraki_api.find_mac_by_network(args.MAC_ADDRESS)
+    print(f'\nStart searching for {mac} in all the networks in the organization {meraki_api.get_org_name()}\n')
+    client = meraki_api.find_mac_by_network(mac)
 
     if client:
-        device_client = meraki_api.get_device_client_data(client.get("recentDeviceSerial"), args.MAC_ADDRESS)
+        device_client = meraki_api.get_device_client_data(client.get("recentDeviceSerial"), mac)
         print(f'    MAC found: {client.get("description")} ({client.get("mac")})')
         print(f'    Last connected to: {client.get("recentDeviceName")} ({client.get("recentDeviceSerial")})')
         print(f'    Connection details: Port: {device_client.get("switchport")}, VLAN: {device_client.get("vlan")}, IP: {device_client.get("ip")}')
@@ -95,6 +105,9 @@ def main():
         meraki_api.blink_device_leds(client.get('recentDeviceSerial'))
         sleep(20)
         print(f'    Blinking device leds of {client.get("recentDeviceName")} completed.')
+        print('\nFind MAC task completed, hope you found what you searched for :)\n')
+    else:
+        print(f'\n{mac} was not found in any of the networks\n')
 
 
 if __name__ == "__main__":
